3c9939ed4337cf4b7607d2dec40942f3d22ccfe5
gegehprast <36652820+gegehprast@users.noreply.github.com>
2024-02-03 16:42:38 +0700

    Update index.ts with new grid size and buttons

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,88 @@
 import p5 from 'p5'
 import Grid from './Grid'
 import Sand from './Sand'
+import { showFrameRate } from './helper'
 
-const WIDTH = 400
+const WIDTH = 500
 const HEIGHT = 500
-const SIZE = 10
+const SIZE = 5
+const ROWS = Math.floor(WIDTH / SIZE)
+const COLS = Math.floor(HEIGHT / SIZE)
+const ROF = 10
 
 let grid: Grid
 
 let lastCalledTime = Date.now()
 
+let pauseButton: p5.Element
+let nextFrameButton: p5.Element
+let addparticleButton: p5.Element
+
 const sketch = (p: p5) => {
     p.setup = () => {
         p.createCanvas(WIDTH, HEIGHT)
         p.colorMode(p.HSL)
+        p.background(0)
+        p.textAlign(p.CENTER, p.CENTER)
 
-        grid = new Grid(p, SIZE, WIDTH, HEIGHT)
+        grid = new Grid(p, ROWS, COLS, SIZE)
+
+        pauseButton = p.createButton('Pause')
+        pauseButton.position(10, HEIGHT + 10)
+        pauseButton.mousePressed(() => {
+            if (p.isLooping()) {
+                p.noLoop()
+                pauseButton.html('Play')
+            } else {
+                p.loop()
+                pauseButton.html('Pause')
+            }
+        })
+
+        nextFrameButton = p.createButton('Next Frame')
+        nextFrameButton.position(10, HEIGHT + 40)
+        nextFrameButton.mousePressed(() => {
+            p.draw()
+        })
+
+        addparticleButton = p.createButton('Add Particle')
+        addparticleButton.position(10, HEIGHT + 70)
+        addparticleButton.mousePressed(() => {
+            grid.grid[0][0] = new Sand(p)
+            grid.grid[grid.rows - 1][0] = new Sand(p)
+        })
     }
 
     p.draw = () => {
-        p.background(0)
-
-        grid.draw(p)
 
         grid.update()
+
+        grid.draw()
 
         if (p.mouseIsPressed) {
             addSand(p)
         }
+
+        // show frame rate
+        showFrameRate(p)
+
+        p.fill('red')
+        p.rect(0, 40, 100, 40)
+        p.fill(255)
+        p.textSize(20)
+        const x = Math.floor(p.mouseX / grid.size)
+        const y = Math.floor(p.mouseY / grid.size)
+        p.text(`${x}, ${y}`, 50, 60)
     }
 }
 
 const addSand = (p: p5) => {
     const now = Date.now()
 
-    if (now - lastCalledTime >= 100) {
+    if (now - lastCalledTime >= ROF) {
         lastCalledTime = now
         
-        grid.addObject(p, new Sand(p))
+        grid.addParticle(p, new Sand(p))
     }
 }
 
